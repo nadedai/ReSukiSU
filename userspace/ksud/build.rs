d760809ebf5273568b4d2116a535486b894a6b95
YC酱luyancib <luyancib@qq.com>
2026-09-24 16:04:36 +0800

    fmt

--- userspace/ksud/build.rs
+++ userspace/ksud/build.rs
@@ -243,10 +243,7 @@ fn build_mkbootfs(out_directory: &Path) {
             format!("x86_64-linux-android{API_LEVEL}"),
             Path::new("bin/x86_64"),
         ),
-        "riscv64-linux-android" => (
-            format!("riscv64-linux-android35"),
-            Path::new("bin/riscv"),
-        ),
+        "riscv64-linux-android" => (format!("riscv64-linux-android35"), Path::new("bin/riscv")),
         _ => panic!("mkbootfs is not configured for Android target {target}"),
     };
 
